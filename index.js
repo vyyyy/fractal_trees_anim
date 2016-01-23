5bab627e1282b9182a62d50815e802170f0c81c2
vyyyy <vpat5777@uni.sydney.edu.au>
2016-01-24 08:32:38 +1100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ window.onload = function() {
 	var width = canvas.width;
 	var height = canvas.height;
 
- // set rate to be less than default 60fps
- var framesPerSecond = 20;
+ // set rate to be less than default 60fps - <20 slow, >20 fast
+       var framesPerSecond = 10;
 
 	// variables (properties of trunk) initialised with an empty array
 	var length, divergence, reduction, line_width, start_points = [];
@@ -17,35 +17,42 @@ window.onload = function() {
 		// canvas
 		ctx.fillStyle = "black";
 		ctx.fillRect(0, 0, width, height);
+		
+ // let's draw the fractal tree by
+		// calling the trunk function
+		trunk();
 
-		// length of the trunk
-		length = 100 + Math.round(Math.random()*50);
+		// calling the branches function
+		branches();
+	}
+	
+	function trunk() {
+                // length of the trunk - range betwen 100 and 150 px
+                length = 100 + Math.round(Math.random()*50);
 
 		// width of the trunk
 		line_width = 15;
 
-		// angle at which branches diverge
+		// angle at which individual branches diverge - range between 10 and 70 degrees
 		divergence = 10 + Math.round(Math.random()*60);
 
 		// every branch will be 0.75times that of the previous one
 		reduction = Math.round(5 + Math.random()*2)/10;
 
-		// the end point of the trunk where smaller branches diverge
+		// branches diverge 90 degrees to the top point of the trunk - constant
 		var trunk = {x: width/2, y: length + 100, angle: 90};
 
 		// empty the start points on every init();
 		start_points = [];
 		start_points.push(trunk);
 
-		// draw fractal tree on canvas
+		// draw trunk on canvas
 		ctx.beginPath();
 		ctx.moveTo(trunk.x, height-50);
 		ctx.lineTo(trunk.x, height-trunk.y);
 		ctx.strokeStyle = "orange";
 		ctx.lineWidth = line_width;
 		ctx.stroke();
-		// call branches function
-		branches();
 	}
 
 	// function to draw branches
@@ -55,37 +62,44 @@ window.onload = function() {
 		line_width = line_width * reduction;
 		ctx.lineWidth = line_width;
 
-		var new_start_points = [];
+		// drawing the branches
 		ctx.beginPath();
+                var new_start_points = [];
 		for(var i = 0; i < start_points.length; i++) {
 			var sp = start_points[i];
 			// 2 branches diverge at every start point
 			var ep1 = get_endpoint(sp.x, sp.y, sp.angle+divergence, length);
 			var ep2 = get_endpoint(sp.x, sp.y, sp.angle-divergence, length);
 
-			// drawing the branches
+			// right hand branch
 			ctx.moveTo(sp.x, height-sp.y);
 			ctx.lineTo(ep1.x, height-ep1.y);
+			// left hand branch
 			ctx.moveTo(sp.x, height-sp.y);
 			ctx.lineTo(ep2.x, height-ep2.y);
 
 			// recursive function to draw more branches
 			ep1.angle = sp.angle+divergence;
 			ep2.angle = sp.angle-divergence;
-
+                        
+                        // push endpoints of both branches to array
 			new_start_points.push(ep1);
 			new_start_points.push(ep2);
 		}
+		
+		// end point of a branch becomes the start point of another branch
+		start_points = new_start_points;
 
 		// set colour of branches
 		if (length < 10) {
+      // short branches
       ctx.strokeStyle = "silver";
     } else {
+      // long branches
       ctx.strokeStyle = "gold";
     }
-
+		// draw on the canvas
 		ctx.stroke();
-		start_points = new_start_points;
 
 		// use rAF and set frame rate
     setTimeout(function() {
